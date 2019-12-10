15ade5ae8337c51136000efd4f0dd509ef9614a3
Dane Johnson <daneallenjohnson@protonmail.com>
2019-12-10 15:01:45 -0600

    Player is kinematic

--- _Scenes/Player.gd
+++ _Scenes/Player.gd
@@ -1,4 +1,4 @@
-extends Spatial
+extends KinematicBody
 
 export var speed = 1.0
 export var turn_speed = 0.1
@@ -6,7 +6,7 @@ export var turn_speed = 0.1
 
 var velocity = Vector3.ZERO
 
-func _process(delta):
+func _physics_process(delta):
 	var deltaVelocity = Vector3.ZERO
 	if Input.is_action_pressed("ui_up"):
 		deltaVelocity += Vector3.FORWARD
@@ -16,6 +16,7 @@ func _process(delta):
 		deltaVelocity += Vector3.LEFT
 	if Input.is_action_pressed("ui_right"):
 		deltaVelocity += Vector3.RIGHT
+	deltaVelocity = deltaVelocity.rotated(Vector3.UP, rotation.y)
 	
 	var dragCoeff = 0.01
 	var fricCoeff = 0.05
@@ -24,22 +25,23 @@ func _process(delta):
 	if velocity.length() == 0:
 		magnitude = 0.00001
 	var dragRatio = drag/magnitude
-	
+
 	if velocity.z < 0:
 		velocity.z += fricCoeff * delta
 	elif velocity.z > 0:
 		velocity.z -= fricCoeff * delta
-		
+
 	if velocity.x < 0:
 		velocity.x += fricCoeff * delta
 	elif velocity.x > 0:
 		velocity.x -= fricCoeff * delta
-	
+
 	velocity = velocity * (1-dragRatio)
 	velocity += deltaVelocity * delta
-	translate_object_local(velocity * delta * speed)
+
+	move_and_collide(velocity * delta * speed)
 
 func _input(event):
 	if event is InputEventMouseMotion:
 		rotate_object_local(Vector3.UP, event.relative[0] * -1 * turn_speed)
-		$Camera.rotate_x(event.relative[1] * -1 *  turn_speed) 
+		$Camera.rotate_x(event.relative[1] * -1 *  turn_speed)
