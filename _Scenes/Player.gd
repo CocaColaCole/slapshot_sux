39a59b7888c3986c19d8291d7e0ed4c0ea5e605a
Dane Johnson <daneallenjohnson@protonmail.com>
2019-12-09 23:15:23 -0600

    player movement

---
+++ _Scenes/Player.gd
@@ -0,0 +1,24 @@
+extends Spatial
+
+export var speed = 1.0
+export var turn_speed = 0.1
+
+func _ready():
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+
+func _process(delta):
+	var velocity = Vector3.ZERO
+	if Input.is_action_pressed("ui_up"):
+		velocity += Vector3.FORWARD
+	if Input.is_action_pressed("ui_down"):
+		velocity -= Vector3.FORWARD
+	if Input.is_action_pressed("ui_left"):
+		velocity += Vector3.LEFT
+	if Input.is_action_pressed("ui_right"):
+		velocity += Vector3.RIGHT
+	translate_object_local(velocity * delta * speed)
+
+func _input(event):
+	if event is InputEventMouseMotion:
+		print(event.as_text())
+		rotate_object_local(Vector3.UP, event.relative[0] * -1 * turn_speed)
