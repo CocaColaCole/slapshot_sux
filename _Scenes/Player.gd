db96cb1794492b5be031509a8729edb7bc5687c3
Dane Johnson <daneallenjohnson@protonmail.com>
2019-12-10 20:00:11 -0600

    Use more kinematics

--- _Scenes/Player.gd
+++ _Scenes/Player.gd
@@ -2,12 +2,30 @@ extends KinematicBody
 
 export var speed = 1.0
 export var turn_speed = 0.1
+export var mass = 0.1
 
+var puck
+var camera_default
 
 var velocity = Vector3.ZERO
+var stick_move = false
+var puck_look = false
+
+func _ready():
+	camera_default = $Camera.transform
+	puck = get_tree().current_scene.find_node("Puck")
+
+func _process(delta):
+	if puck_look:
+		$Camera.look_at(puck.translation, Vector3.UP)
 
 func _physics_process(delta):
-	var deltaVelocity = Vector3.ZERO
+	var stick_pos = $HockeyStick.translation
+	stick_pos.x = clamp(stick_pos.x, -.5, .5)
+	stick_pos.y = clamp(stick_pos.y, -.2, .1)
+	$HockeyStick.translation = stick_pos
+	
+	var deltaVelocity = Vector3.DOWN * mass
 	if Input.is_action_pressed("ui_up"):
 		deltaVelocity -= transform.basis.z
 	if Input.is_action_pressed("ui_down"):
@@ -37,10 +55,24 @@ func _physics_process(delta):
 
 	velocity = velocity * (1-dragRatio)
 	velocity += deltaVelocity * delta
-
-	move_and_collide(velocity * delta * speed)
+	
+	var collision = move_and_collide(velocity * delta * speed, true)
 
 func _input(event):
+	if event is InputEventMouseButton:
+		if event.button_index == BUTTON_RIGHT:
+			stick_move = event.pressed
+	if event is InputEventKey:
+		if event.scancode == KEY_SPACE and event.pressed:
+			if puck_look:
+				puck_look = false
+				$Camera.transform = camera_default
+			else:
+				puck_look = true
 	if event is InputEventMouseMotion:
-		rotate_object_local(Vector3.UP, event.relative[0] * -1 * turn_speed)
-		$Camera.rotate_x(event.relative[1] * -1 *  turn_speed)
+		if stick_move:
+			$HockeyStick.move_and_collide(transform.basis.x * event.relative[0] * 0.01)
+			$HockeyStick.move_and_collide(Vector3.UP * event.relative[1] * -0.01)
+		else:
+			rotate_object_local(Vector3.UP, event.relative[0] * -1 * turn_speed)
+			$Camera.rotate_x(event.relative[1] * -1 *  turn_speed)
